ccb1212620393234819d690fec57904ad4b126a2
Sourav Kumar Saha <souravsaha.prgmr@gmail.com>
2024-06-07 13:56:03 +0600

    login/route added

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express=require('express');
 const cors=require('cors');
 const { default: mongoose } = require('mongoose');
 
+const bcrypt=require('bcrypt')
+
 const User=require('./models/User_model')
 
 const app=express()
+
+const salt=bcrypt.genSaltSync(10)
 
 app.use(cors());
 app.use(express.json())
@@ -16,12 +20,25 @@ app.post('/register',async (req,res)=>{
 
     const {fullname,email,password}=req.body
     try{
-        const userDoc=await User.create({fullname,email,password})
+        const userDoc=await User.create({
+            fullname,
+            email,
+            password:bcrypt.hashSync(password,salt)
+        })
         res.json(userDoc);
     }catch(e){
         res.status(400).json(e)
     }
     
+})
+
+app.post('/login',async(req,res)=>{
+    const {email,password}=req.body
+    const userDoc=await User.findOne({email})
+    const passOK=bcrypt.compareSync(password,userDoc.password)
+
+    res.json(passOK)
+
 })
 
 
